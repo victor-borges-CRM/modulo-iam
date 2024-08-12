a00c745ff6d247d16b9bbecbe9b90b0c4a06f735
Victor-Borges-Silva <victorborges975@gmail.com>
2024-08-12 15:53:54 -0300

    ajuste nas variaveis para incluir modulo root

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 data "aws_iam_role" "importa_funcao_iam" {
-  name = "PermiteSSMGenrenciarEC2"
+  name = var.nome_role_iam
 }
 
 resource "aws_iam_instance_profile" "perfil_ec2" {
-  name = "test_perfil_ec2"
+  name = var.nome_instance_profile
   role = data.aws_iam_role.importa_funcao_iam.name
 }
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "nome_role_iam" {
+  type        = string
+  default     = "Default_TF"
+  description = "Nome da funcao IAM a ser importada"
+}
+
+variable "nome_instance_profile" {
+  type        = string
+  default     = "Default_TF"
+  description = "Nome da funcao IAM a ser importada"
+}
